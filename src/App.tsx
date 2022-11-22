9906133e3e7c7275f29202da4557a6c4d7caec3a
Marcos Victor <marcos_victor159@hotmail.com>
2022-11-22 20:31:46 -0300

    feito a lista com uma function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Button from './components/buttons';
 import Formulario from './components/formulario';
+import Lista from './components/lista';
 
 function App() {
   return (
     <div className="App">
       <Formulario/>
+      <Lista/>
     </div>
   );
 }
